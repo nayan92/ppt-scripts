73c26308759eb64ea8f0e7f73316e3b29c0e0283
Nayan Chudasama <nayan.chudasama11@gmail.com>
2013-01-09 16:39:22 +0000

    Now checks for the -p option (to print) (currently not working, just prints messages)#

--- getScripts.sh
+++ getScripts.sh
@@ -8,33 +8,72 @@
 
 # Second argument is the name which you would like to call the directory where the submissions will be saved.
 
+#
+# This function will print to the screen instructions on how to
+# use the command.
+# TODO: add what the -p option means
+#
 function usage_instructions {
-  echo "usage: getScripts [-p] exercise store_folder_name"
+  case $1 in
+    1)
+      echo "usage: getScripts [-p] exercise store_folder_name"
+      ;;
+    2)
+      echo "incorrect usage"
+      ;;
+  esac
 }
 
-if [ -z "$1" ]; then
-  usage_instructions
+# True if required to print the submissions
+# False otherwise
+to_print=false
+
+# If there are no arguments, print usage instructions.
+# Otherwise get the scripts.
+if [ $# -eq 0 ]; then
+  usage_instructions 1
 else
-  # This is the path to the folder where you store all the submissions.
-  storeDir=~/Documents/ThirdYear/PPT-UTA/$2
 
-  echo "Creating target directory..."
-  mkdir $storeDir
-
-  echo "Downloading submissions archive..."
-  wget -O $storeDir/autotest.tgz --user=nc1610 --ask-password https://www.doc.ic.ac.uk/~tora/firstyear/ppt/group/2/exercise/$1/autotest.tgz
-
-  echo "Extracting archive..."
-  tar -xf $storeDir/autotest.tgz -C $storeDir
-
-  echo "Removing archive..."
-  rm -f $storeDir/autotest.tgz
-
-  echo "Printing submissions..."
-  for f in $storeDir/*.pdf
-  do
-    lpr -P ICTMono -o HPStaplerOptions=1diagonal $f
+  while getopts ":p" opt; do
+    case $opt in
+      p)
+        to_print=true
+        echo "-p was triggered"
+        ;;
+      \?)
+        echo "invalid option"
+        ;;
+    esac
   done
 
-  echo "Complete!"
+  shift $((OPTIND-1))
+  echo "argument 1 is: $1"
+
+  echo "Scripts downloaded"
+
+  if $to_print; then
+    echo "Scripts printed"
+  fi
+#  # This is the path to the folder where you store all the submissions.
+#  storeDir=~/Documents/ThirdYear/PPT-UTA/$2
+
+#  echo "Creating target directory..."
+#  mkdir $storeDir
+
+#  echo "Downloading submissions archive..."
+#  wget -O $storeDir/autotest.tgz --user=nc1610 --ask-password https://www.doc.ic.ac.uk/~tora/firstyear/ppt/group/2/exercise/$1/autotest.tgz
+
+#  echo "Extracting archive..."
+#  tar -xf $storeDir/autotest.tgz -C $storeDir
+
+#  echo "Removing archive..."
+#  rm -f $storeDir/autotest.tgz
+
+#  echo "Printing submissions..."
+#  for f in $storeDir/*.pdf
+#  do
+#    lpr -P ICTMono -o HPStaplerOptions=1diagonal $f
+#  done
+
+#  echo "Complete!"
 fi
